8c8b9f0e392dcb2f734caace186b90a732e1bfdf
newrodrigo <rodrigocerqueira@outlook.com>
2023-05-06 23:01:55 -0300

    Custom content for current user

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -1,11 +1,10 @@
 package com.devsuperior.dslearnbds.resources;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
-import com.devsuperior.dslearnbds.dto.UserDTO;
-import com.devsuperior.dslearnbds.entities.User;
 import com.devsuperior.dslearnbds.services.NotificationService;
-import com.devsuperior.dslearnbds.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,15 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/notifications")
+public class NotificationResource {
 
     @Autowired
-    private UserService service;
+    private NotificationService service;
 
-    @GetMapping(value ="/{id}")
-    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
-        UserDTO dto = service.findById(id);
-        return ResponseEntity.ok().body(dto);
+    @GetMapping
+    public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(Pageable pageable) {
+        Page<NotificationDTO> page = service.notificationsForCurrentUser(pageable);
+        return ResponseEntity.ok().body(page);
     }
 }
